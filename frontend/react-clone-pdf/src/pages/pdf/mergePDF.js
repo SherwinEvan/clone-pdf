83ee20dd5ca99ca298997ed1c0c5ee218a040776
Sherwin V Evan <sherwinvevan@outlook.com>
2023-06-20 18:04:47 +0530

    UI improvements

--- frontend/react-clone-pdf/src/pages/pdf/mergePDF.js
+++ frontend/react-clone-pdf/src/pages/pdf/mergePDF.js
@@ -6,6 +6,7 @@ import NavBar from "../../components/navbar";
 import Footer from "../../components/footer";
 import CallMergeIcon from "@mui/icons-material/CallMerge";
 import DownloadIcon from "@mui/icons-material/Download";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { MuiFileInput } from "mui-file-input";
 import { saveAs } from "file-saver";
 import { PDFDocument } from "pdf-lib";
@@ -26,7 +27,11 @@ export default function CompressPDF() {
   const [selectedFiles, setSelectedFiles] = useState([]);
 
   const handleChange = (newFiles) => {
-    if (newFiles.length > 0 && newFiles[0].type === "application/pdf") {
+    if (
+      newFiles &&
+      newFiles.length > 0 &&
+      newFiles[0].type === "application/pdf"
+    ) {
       setPDFFile([...pdfFile, ...newFiles]);
       setSelectedFiles([...selectedFiles, ...newFiles]);
       setError(null);
@@ -39,15 +44,16 @@ export default function CompressPDF() {
   const handleFileUpload = async () => {
     setError(null);
 
-    if (pdfFile.length === 0) {
-      setError("Please select a PDF file.");
+    if (pdfFile.length <= 1) {
+      setError("Please select at least 2 PDF files.");
       return;
     }
 
     try {
       const mergedPdf = await PDFDocument.create();
+      const totalFiles = pdfFile.length;
 
-      for (let i = 0; i < pdfFile.length; i++) {
+      for (let i = 0; i < totalFiles; i++) {
         const file = pdfFile[i];
         const reader = new FileReader();
 
@@ -57,7 +63,15 @@ export default function CompressPDF() {
 
         reader.onprogress = (event) => {
           if (event.lengthComputable) {
-            const progress = Math.round((event.loaded / event.total) * 100);
+            const loaded =
+              Array.from(pdfFile)
+                .slice(0, i)
+                .reduce((acc, f) => acc + f.size, 0) + event.loaded;
+            const total = Array.from(pdfFile).reduce(
+              (acc, f) => acc + f.size,
+              0
+            );
+            const progress = Math.round((loaded / total) * 100);
             setUploadProgress(progress);
           }
         };
@@ -89,6 +103,12 @@ export default function CompressPDF() {
     } catch (error) {
       console.error("Error merging PDFs:", error);
     }
+  };
+
+  const handleDeleteFile = (index) => {
+    const updatedFiles = [...pdfFile];
+    updatedFiles.splice(index, 1);
+    setPDFFile(updatedFiles);
   };
 
   const onSubmit = (data) => {
@@ -137,7 +157,7 @@ export default function CompressPDF() {
               </Button>
             </div>
           </div>
-          {!pdfFile && error && (
+          {error && (
             <span className="flex justify-around text-red-500 text-sm mx-5">
               {error}
             </span>
@@ -147,6 +167,42 @@ export default function CompressPDF() {
           </div>
         </form>
       </div>
+
+      <div>
+        {pdfFile.length > 0 && (
+          <div className="my-5 mx-5 md:mx-12 md:px-12">
+            <div className="font-semibold text-lg mb-2 text-center">
+              Selected PDFs
+            </div>
+            <table className="table-fixed">
+              <thead>
+                <tr>
+                  <th className="w-2/3 text-left">File Name</th>
+                  <th className="w-1/3 text-left">File Size</th>
+                  <th className="text-left">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {pdfFile.map((file, index) => (
+                  <tr key={index} className="border-b border-gray-200">
+                    <td className="py-2 w-2/3">{file.name}</td>
+                    <td className="py-2 w-1/3">
+                      {(file.size / (1024 * 1024)).toFixed(2)} MB
+                    </td>
+                    <td className="py-2 text-center">
+                      <DeleteIcon
+                        className="text-red-500 hover:text-red-700 cursor-pointer"
+                        onClick={() => handleDeleteFile(index)}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
+
       <div className="flex flex-col justify-evenly md:pt-5 md:mt-0 md:px-10 md:mx-5 mx-6 mt-5 shadow">
         <div>
           {uploadProgress > 0 && (
